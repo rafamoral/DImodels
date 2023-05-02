9e6bf64f14352e8f84aac9c9d954e16b07ba9eaf
rafamoral <rafael_moral@yahoo.com.br>
2023-05-02 16:11:26 +0100

    updated richness_vs_DI function

--- R/richness_vs_DI.R
+++ R/richness_vs_DI.R
@@ -30,39 +30,53 @@ richness_vs_DI <- function(y, prop, data, extra_formula) {
   formula_richness <- formula(paste(y, "~ richness", extra_terms))
   m1 <- lm(formula_richness, data = newdata)
   
-  ## model 2: the DI equivalent
+  ## model 2: the DI equivalent (equal identity effects and fixing theta = 0.5)
   formula_DI1 <- formula(paste(y, "~ AV0.5", extra_terms))
   m2 <- lm(formula_DI1, data = newdata)
   
-  ## model 3: allowing different identity effects
-  m3 <- suppressWarnings(
+  ## model 3: equal identity effects and estimating theta
+  theta_hat <- suppressWarnings(
+    suppressMessages(
+      coef(DI(y = y, prop = prop, DImodel = "AV", data = data,
+              estimate_theta = TRUE, extra_formula = extra_formula))["theta"]
+    ))
+  newdata$AV_theta <- DI_data(prop = prop, data = data, what = "AV", theta = theta_hat)
+  formula_m3 <- formula(paste(y, "~ AV_theta", extra_terms))
+  m3 <- lm(formula_m3, data = newdata)
+  m3$df.residual <- m3$df.residual - 1
+  m3$rank <- m3$rank + 1
+  
+  ## model 4: allowing different identity effects and fixing theta = 0.5
+  m4 <- suppressWarnings(
     suppressMessages(
       DI(y = y, prop = prop, DImodel = "AV", data = data, theta = 0.5,
          extra_formula = extra_formula)
       ))
-  m3$df.residual <- m3$df.residual + 1
+  m4$df.residual <- m4$df.residual + 1
   
-  ## model 4: estimating theta
-  m4 <- suppressWarnings(
+  ## model 5: allowing different identity effects and estimating theta
+  m5 <- suppressWarnings(
     suppressMessages(
       DI(y = y, prop = prop, DImodel = "AV", data = data, estimate_theta = TRUE,
          extra_formula = extra_formula)
     ))
   
   ## get AIC values for the four models
-  all_AICs <- c(AIC(m1), AIC(m2), AIC(m3), AIC(m4))
-  model_list <- list(m1, m2, m3, m4)
+  all_AICs <- c(AIC(m1), AIC(m2), AIC(m3), AIC(m4), AIC(m5))
+  model_list <- list(m1, m2, m3, m4, m5)
   
   ## print AIC values
   the_table <- data.frame("AIC" = all_AICs,
                           "df" = n - c(m1$df.residual,
                                        m2$df.residual,
                                        m3$df.residual,
-                                       m4$df.residual),
+                                       m4$df.residual,
+                                       m5$df.residual),
                           "Description" = c("Model 1: Richness only",
                                             "Model 2: Average interactions 'AV' DImodel with common identity effects and theta = 0.5",
-                                            "Model 3: Average interactions 'AV' DImodel with unique identity effects and theta = 0.5",
-                                            "Model 4: Average interactions 'AV' DImodel with unique identity effects and theta estimated"),
+                                            "Model 3: Average interactions 'AV' DImodel with common identity effects and theta estimated",
+                                            "Model 4: Average interactions 'AV' DImodel with unique identity effects and theta = 0.5",
+                                            "Model 5: Average interactions 'AV' DImodel with unique identity effects and theta estimated"),
                           row.names = NULL)
   
   message("\n", strrep("-", getOption("width")))
@@ -71,7 +85,7 @@ richness_vs_DI <- function(y, prop, data, extra_formula) {
   
   ## return the model with smallest AIC
   message("\n", strrep("-", getOption("width")))
-  message("richness_vs_DI is limited in terms of model selection. Only four models are explored. See ?autoDI and ?DI for more options.")
+  message("Models 1 and 2 are equivalent if all mixtures in the dataset at any given level of richness are equi-proportional.\nrichness_vs_DI is limited in terms of model selection. Only five models are explored. See ?autoDI and ?DI for more options.")
   message(strrep("-", getOption("width")))
   return(invisible(model_list[[which.min(all_AICs)]]))
 }
